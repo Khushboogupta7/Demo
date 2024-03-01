c7770110620b4b0292f7af204f8435f775a4ad28
Khushboo <reachtokhushboogupta@gmail.com>
2024-03-01 16:28:20 +0530

    inserted values in tables

--- First.sql
+++ First.sql
@@ -21,3 +21,25 @@ FROM EmployeeDetails
 JOIN Employeesalary
 ON EmployeeDetails.Emp_id = Employeesalary.Emp_id
 WHERE Salary> 50000;
+
+-- Inserting Values Into Table.
+INSERT INTO EmployeeDetails
+VALUES
+	(121, 'John Snow', 321, '2019-01-31' , 'Toronto'),
+	(321, 'Walter White', 986, '2020-01-30' , 'California'),
+	(421, 'Kuldeep Rana', 876, '2021-11-27' , 'New Delhi');
+
+INSERT INTO Employeesalary
+VALUES
+	(121, 'P1', 8000 , 500),
+	(321, 'P2' , 10000 , 1000),
+	(421, 'P1', 12000 , 0);
+    
+-- 1. Write an SQL query to fetch the EmpId and FullName of 
+-- all the employees working under the Manager with id – ‘986’.
+
+SELECT Emp_id, Fullname 
+FROM EmployeeDetails
+WHERE Manager_id =986;
+
+
